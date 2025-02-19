b88247653f3372b3ab735d2999ec649bb46d666b
Ismael Treviño <60386407+Ismola@users.noreply.github.com>
2025-02-19 21:19:15 +0000

    fix(Main): :bug: / app route response

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return 'selenium-scraper-quickstarter'
 
 
 @app.route('/sample', methods=['GET'])
