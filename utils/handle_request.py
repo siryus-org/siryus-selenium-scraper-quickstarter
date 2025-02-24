e4c5f324ba77fad2ad0209247264c65c42cfd800
Ismael Treviño <ismolahiena@gmail.com>
2025-02-24 11:56:43 +0000

    style(Handle Request):

--- utils/handle_request.py
+++ utils/handle_request.py
@@ -9,11 +9,9 @@ from utils.security import authenticate_token
 def handle_request_endpoint(controller_function):
     configure_logger()
     start_time = time.time()
-
     logging.info("|| Controller:" + controller_function.__name__)
     if STAGE != "production":
         print("|| Controller:" + controller_function.__name__)
-
     if not authenticate_token():
         return jsonify({"status": "ERROR", "message": "Unauthorized", "time": time.time() - start_time}), 401
 
@@ -22,13 +20,11 @@ def handle_request_endpoint(controller_function):
 
     try:
         data = request.json
-
         logging.info(
             {key: value for key, value in data.items() if key != 'password'})
         message = controller_function(data)
         logging.info(f"OK - message: {message}")
         return jsonify({"status": "OK", "message": message, "time": time.time() - start_time}), 200
-
     except Exception as e:
         error_message = str(e)
         logging.error(f"ERROR: {error_message}")
