fa3c2d7e6185d0094ac4904719a3474b81d5efa3
Ismael Treviño <ismolahiena@gmail.com>
2025-02-25 11:20:16 +0000

    feat(write Element has slow mode): :sparkles:

--- actions/write_element.py
+++ actions/write_element.py
@@ -1,16 +1,24 @@
+
 import inspect
+from time import sleep
 from utils.error import messageError
 from selenium.webdriver.common.action_chains import ActionChains
 
 
 # This function searches for an element on the page, scrolls to it, and writes to it.
-def write_element(driver, element, text, clear=True):
+def write_element(driver, element, text, clear=True, slow=False):
     try:
+
         actions = ActionChains(driver)
         actions.move_to_element(element).perform()
         if clear:
             element.clear()
-        element.send_keys(text)
+        if slow:
+            for i in text:
+                sleep(0.1)
+                element.send_keys(i)
+        else:
+            element.send_keys(text)
         return driver
     except Exception as e:
         raise messageError(
