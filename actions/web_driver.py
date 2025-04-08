09ee1e16d5c448597aff747b175c553e19e8fe42
ismola <ismolahiena@gmail.com>
2025-04-08 09:17:28 +0000

    feat(web driver): :sparkles: Add Firefox browser option

--- actions/web_driver.py
+++ actions/web_driver.py
@@ -5,73 +5,33 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from utils.config import PAGE_MAX_TIMEOUT, URL, DOWNLOAD_DIR, has_display
+
 import psutil
 
 
-def get_driver():
-    route = ChromeDriverManager().install()
+def get_driver_chrome():
     options = Options()
-
-    if not has_display():
-        options.add_argument("--headless")
-
-    # Variables to not be detected as a bot
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
-    options.add_argument(f"user-agent={user_agent}")
-    options.add_argument("--disable-web-security")
-    options.add_argument("--disable-extension")
-    options.add_argument("--disable-notifications")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--password-store=basic")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--log-level=3")
-    options.add_argument("--allow-running-insecure-content")
-    options.add_argument("--no-default-browser-check")
-    options.add_argument("--no-first-run")
-    options.add_argument("--no-proxy-server")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-cache")
-    options.add_argument("--disable-translate")
-
-    exp_opt = [
-        # Disable possible errors
-        "enable_automation",
-        "ignore-certificate-errors",
-        "enable-logging"
-    ]
-    options.add_experimental_option("excludeSwitches", exp_opt)
-
-    pref_opt = {
-        # Disable all type of popups
-        "profile.default_content_setting_values.notifications": 2,
-        "profile.password_manager_enabled": False,
-        "intl.accept_languages": ["es-Es", "es"],
-        "credentials_enable_service": False,
-
-        # Automatic downloads
-        "download.default_directory": DOWNLOAD_DIR,
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        # Optional: Open PDFs in a separate viewer
-        "plugins.always_open_pdf_externally": True
-    }
-    options.add_experimental_option("prefs", pref_opt)
-
-    # Init driver
-    service = Service(route)
-    driver = webdriver.Chrome(service=service, options=options)
+    options = add_generic_arguments(options)
+    options = add_chrome_arguments(options)
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=options)
     return driver
 
 
-def get_page():
+def get_driver_firefox():
+    options = webdriver.FirefoxOptions()
+    options = add_generic_arguments(options)
+    driver = webdriver.Firefox(options=options)
+    return driver
+
+
+def get_page(browser = 'chrome'):
     # Return driver
     logging.info('Starting driver')
-    driver = get_driver()
+    if browser == 'firefox':
+        driver = get_driver_firefox()
+    else:
+        driver = get_driver_chrome()
     logging.info('Getting URL')
     driver.get(URL)
     return driver
@@ -95,3 +55,57 @@ def kill_driver_process():
                 proc.kill()
         except psutil.NoSuchProcess:
             pass
+
+
+def add_generic_arguments(options):
+    if not has_display():
+        options.add_argument("--headless")
+    options.add_argument("--disable-web-security")
+    options.add_argument("--disable-extension")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--password-store=basic")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--allow-running-insecure-content")
+    options.add_argument("--no-default-browser-check")
+    options.add_argument("--no-first-run")
+    options.add_argument("--no-proxy-server")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-popup-blocking")
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-cache")
+    options.add_argument("--disable-translate")
+    return options
+
+
+def add_chrome_arguments(options):
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+    options.add_argument(f"user-agent={user_agent}")
+    options.add_argument("--log-level=3")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    exp_opt = [
+        # Disable possible errors
+        "enable_automation",
+        "ignore-certificate-errors",
+        "enable-logging"
+    ]
+    options.add_experimental_option("excludeSwitches", exp_opt)
+    pref_opt = {
+        # Disable all type of popups
+        "profile.default_content_setting_values.notifications": 2,
+        "profile.password_manager_enabled": False,
+        "intl.accept_languages": ["es-Es", "es"],
+        "credentials_enable_service": False,
+
+        # Automatic downloads
+        "download.default_directory": DOWNLOAD_DIR,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        # Optional: Open PDFs in a separate viewer
+        "plugins.always_open_pdf_externally": True
+    }
+    options.add_experimental_option("prefs", pref_opt)
+
+    return options
